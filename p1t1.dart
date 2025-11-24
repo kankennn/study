af6fa6149573c7f76ec57a204cef8c3d85681c19
kankennn <sskanken@gmail.com>
2025-11-24 20:38:20 +0500

    Add files via upload
    p - номер практической
    t - номер задачи

---
+++ p1t1.dart
@@ -0,0 +1,8 @@
+void main() {
+    String name = "Радислав";
+    int years = 18;
+    double avg = 4.5;
+    String student = "Да";
+    print("\n Имя: $name String \n Возраст: $years int \n Средний балл: $avg double \n Студент: $student string");
+    
+}
---
+++ p1t2.dart
@@ -0,0 +1,16 @@
+void main() {
+  for(int i = 1; i < 101; i++){
+    if (i%3==0 && i%5==0){
+      print("$i FizzBuzz");
+    }
+    else if (i%3==0){
+      print("$i Fizz");
+    }
+    else if (i%5==0){
+      print("$i Buzz");
+    }
+    else{
+      print("$i");
+    }
+  }
+}
