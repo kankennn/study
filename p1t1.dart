3305cfd3f35092f80fc306e6679b94e81c7f69e6
kankennn <sskanken@gmail.com>
2025-11-24 21:08:57 +0500

    Delete p1t1.dart

--- p1t1.dart
+++
@@ -1,8 +0,0 @@
-void main() {
-    String name = "Радислав";
-    int years = 18;
-    double avg = 4.5;
-    String student = "Да";
-    print("\n Имя: $name String \n Возраст: $years int \n Средний балл: $avg double \n Студент: $student string");
-    
-}
